69d2f2030fa3813ab73198f480e6f27051ea7d79
ezreum <whismaster_power@hotmail.com>
2020-10-13 12:51:25 +0200

    restserver ocultas keys

--- 07-RESTServer/server/config/config.js
+++ 07-RESTServer/server/config/config.js
@@ -12,14 +12,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //ruta local
 //mongodb://localhost:27017/cafe
-// remota mongodb+srv://ezreum:pwd@cluster0.ubyle.mongodb.net/cafe
+// remota mongodb+srv://user:pwd@cluster0.ubyle.mongodb.net/cafe
 
 let urlDB;
 
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://ezreum:897qSCZocU2TPVSV@cluster0.ubyle.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
